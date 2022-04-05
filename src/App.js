61853a9341fe61437fbcbcec656a5a240c9f11fe
Rafikhan15 <mdrafi.khan0089@gmail.com>
2022-04-05 19:36:52 +0600

    home page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 
 function App() {
@@ -7,7 +11,12 @@ function App() {
     <div className="App">
       <Header></Header>
 
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
 
+
+      </Routes>
 
     </div>
   );
