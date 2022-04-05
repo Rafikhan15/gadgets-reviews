a2217498255f55e48493cfd05ac6ebf710bde9c1
Rafikhan15 <mdrafi.khan0089@gmail.com>
2022-04-05 19:42:54 +0600

    Not found page added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-
+import NotFound from './components/NotFound/NotFound';
 import Review from './components/Review/Review';
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/review' element={<Review></Review>}></Route>
 
+        <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
 
